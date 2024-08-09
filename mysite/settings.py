dfe426b41c0386415b4f36ce9381307bb172157d
Tiru <thiruvenganna@gmail.com>
2024-08-09 18:16:43 +0530

    bug fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #'members',
-    #"debug_toolbar",
+    "debug_toolbar",
     'rest_framework_simplejwt',
     'carshops',
     'rest_framework',
