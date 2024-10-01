38170b4127d14f3981fb6e2c458233bfff6520cd
Tiru <thiruvenganna@gmail.com>
2024-10-01 22:43:03 +0530

    Fix API

--- mysite/settings.py
+++ mysite/settings.py
@@ -183,7 +183,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'thiruvenganna@gmail.com'  # Your email
-EMAIL_HOST_PASSWORD = 'ogxynrzfrhikchcl'  # Your email password
+EMAIL_HOST_USER = ''  # Your email
+EMAIL_HOST_PASSWORD = ''  # Your email password
 
 
